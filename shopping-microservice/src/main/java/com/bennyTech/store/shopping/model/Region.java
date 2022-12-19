05f5095624061f04b7e6b0cc287a1c01c591e973
bennedito01@gmail.com <bennedito01@gmail.com>
2022-12-19 16:58:48 +0200

    actualizacao do projecto e organizacao

--- shopping-microservice/src/main/java/com/bennyTech/store/shopping/model/Region.java
+++
@@ -1,9 +0,0 @@
-package com.bennyTech.store.shopping.model;
-
-import lombok.Data;
-
-@Data
-public class Region {
-    private Long id;
-    private String name;
-}
--- shopping-microservice/src/main/java/com/bennyTech/store/shopping/repository/InvoiceItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bennyTech.store.shopping.repository;
-
-import com.bennyTech.store.shopping.entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem, Long> {
-}
