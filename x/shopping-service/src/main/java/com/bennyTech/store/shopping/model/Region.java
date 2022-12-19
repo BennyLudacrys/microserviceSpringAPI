b1965d9e7bf61fc029f3faa8439e1d97a0cdc88f
bennedito01@gmail.com <bennedito01@gmail.com>
2022-12-19 11:49:29 +0200

    shopping-service configuracoes

---
+++ x/shopping-service/src/main/java/com/bennyTech/store/shopping/model/Region.java
@@ -0,0 +1,9 @@
+package com.bennyTech.store.shopping.model;
+
+import lombok.Data;
+
+@Data
+public class Region {
+    private Long id;
+    private String name;
+}
---
+++ x/shopping-service/src/main/java/com/bennyTech/store/shopping/repository/InvoiceItemsRepository.java
@@ -0,0 +1,7 @@
+package com.bennyTech.store.shopping.repository;
+
+import com.bennyTech.store.shopping.entity.InvoiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem,Long> {
+}
